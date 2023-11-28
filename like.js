359ef8fc270f2f888c38e342b3789d08bf9defd3
prakhargaba007 <prakhargaba@gmail.com>
2023-11-28 16:16:54 +0530

    4 commit

--- like.js
+++ like.js
@@ -1,15 +1,59 @@
+// function toggleLike() {
+//     const likeBtn = document.getElementById('likeBtn');
+//     const dislikeBtn = document.getElementById('dislikeBtn');
+
+//     likeBtn.classList.toggle('liked');
+//     dislikeBtn.classList.remove('disliked');
+// }
+
+// function toggleDislike() {
+//     const dislikeBtn = document.getElementById('dislikeBtn');
+//     const likeBtn = document.getElementById('likeBtn');
+
+//     dislikeBtn.classList.toggle('disliked');
+//     likeBtn.classList.remove('liked');
+// }
+
+let likes = parseInt(localStorage.getItem('likes')) || 0;
+let isLiked = localStorage.getItem('isLiked') === 'true';
+
+updateLikeCount();
+
+function updateLikeCount() {
+    document.getElementById('likeCount').innerText = likes;
+}
+
 function toggleLike() {
     const likeBtn = document.getElementById('likeBtn');
-    const dislikeBtn = document.getElementById('dislikeBtn');
 
+    if (isLiked) {
+        likes--;
+    } else {
+        likes++;
+    }
+
+    isLiked = !isLiked;
     likeBtn.classList.toggle('liked');
-    dislikeBtn.classList.remove('disliked');
+
+    localStorage.setItem('likes', likes);
+    localStorage.setItem('isLiked', isLiked);
+
+    updateLikeCount();
 }
 
 function toggleDislike() {
     const dislikeBtn = document.getElementById('dislikeBtn');
-    const likeBtn = document.getElementById('likeBtn');
+
+    if (isLiked) {
+        likes--;
+        isLiked = false;
+        document.getElementById('likeBtn').classList.remove('liked');
+    }
 
     dislikeBtn.classList.toggle('disliked');
-    likeBtn.classList.remove('liked');
+
+    localStorage.setItem('likes', likes);
+    localStorage.setItem('isLiked', isLiked);
+
+    updateLikeCount();
 }
