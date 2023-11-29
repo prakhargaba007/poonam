12e6bb35b487c149aa30e925ee971b007b3fc1f6
prakhargaba007 <prakhargaba@gmail.com>
2023-11-29 11:20:08 +0530

    6 commit

--- like.js
+++ like.js
@@ -13,3 +13,16 @@ function toggleDislike() {
     dislikeBtn.classList.toggle('disliked');
     likeBtn.classList.remove('liked');
 }
+
+var newDiv = document.createElement("div");
+
+// Set attributes or styles if needed
+newDiv.className = "new-component";
+newDiv.innerHTML = `<h1>Poonam Bhatia</h1>
+<nav>
+<a href="index.html">Home</a>
+<a href="page2.html">Prem Ragini</a>
+</nav>`;
+
+// Append the new div to the container
+document.getElementById("header").appendChild(newDiv);
